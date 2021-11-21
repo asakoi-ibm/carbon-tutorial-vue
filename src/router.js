6ce1b388e3550ef82aaf70a91fba0555bd319821
Asako Iwai <asakoi@jp.ibm.com>
2021-11-21 12:20:04 +0900

    feat(tutorial): complete step 1

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import LandingPage from './views/LandingPage';
 
 Vue.use(Router);
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'landing-page',
+      component: LandingPage
     },
     {
       path: '/about',
@@ -19,6 +20,12 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/repos',
+      name: 'repo-page',
+      component: () =>
+        import(/* webpackChunkName: "RepoPage" */ './views/RepoPage')
     }
   ]
 });
